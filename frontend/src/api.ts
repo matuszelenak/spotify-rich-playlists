b50c0e5fad09be0f8272068129081a2c0e4a87a8
Matúš Zeleňák <matus.zelenak.gjh@gmail.com>
2024-01-08 00:03:08 +0100

    Revert back to our method of token handling

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,24 +1,75 @@
 import {QueryClient} from "react-query";
 import axios from "axios";
-import {SpotifyApi} from '@spotify/web-api-ts-sdk';
+import {AccessToken, SpotifyApi} from '@spotify/web-api-ts-sdk';
+import {Dispatch, SetStateAction, useMemo} from "react";
+import type {SdkOptions} from "@spotify/web-api-ts-sdk/src/types";
+import { useLocalStorage } from "@uidotdev/usehooks";
 
 
-export const sdk = SpotifyApi.withUserAuthorization(
-    import.meta.env.VITE_SPOTIFY_CLIENT_ID,
-    `${window.location.protocol}//${window.location.host}/spotify-auth-callback`,
-    [
-        "playlist-read-private",
-        "playlist-read-collaborative",
-        "playlist-modify-private",
-        "playlist-modify-public",
-        "user-read-email",
-        'user-library-modify',
-        'user-library-read'
-    ]
-);
+const scopes = [
+    "playlist-read-private",
+    "playlist-read-collaborative",
+    "playlist-modify-private",
+    "playlist-modify-public",
+    "user-read-email",
+    'user-library-modify',
+    'user-library-read'
+]
+
+
+const useAccessToken = (): [AccessToken, Dispatch<SetStateAction<AccessToken | null>>] => {
+    const [accessToken, setToken] = useLocalStorage<AccessToken | null>('spotify_access_token')
+    //const [launchAuth, setLaunchAuth] = useState(false)
+
+    return useMemo(() => {
+        console.log('token')
+        if (!accessToken) {
+            return [triggerAuthorization(), setToken]
+        }
+
+        return [accessToken, setToken]
+    }, [accessToken])
+}
+
+export const useSpotifyApi = (): [SpotifyApi, string] => {
+   const [accessToken, setAccessToken   ] = useAccessToken()
+
+    const options: SdkOptions = {
+        errorHandler: {
+            handleErrors: async (error: Error) => {
+                if (error.message.includes('expired')) {
+                    const q = new URLSearchParams({
+                        refresh_token: accessToken.refresh_token
+                    })
+                    axiosBackend({
+                        method: 'get',
+                        url: `/refresh-token?${q.toString()}`
+                    }).then(({data}) => {
+                        const newToken = {...accessToken, ...data}
+                        setAccessToken(newToken)
+                        //localStorage.setItem('spotify_access_token', JSON.stringify(newToken))
+                    }).catch((error) => {
+                        setAccessToken(null)
+                        //localStorage.removeItem('spotify_access_token')
+                        triggerAuthorization()
+                    })
+                }
+                return true
+            }
+        }
+    }
+
+    return useMemo(() => {
+        const a = SpotifyApi.withAccessToken(
+            import.meta.env.VITE_SPOTIFY_CLIENT_ID,
+            accessToken,
+            options
+        )
+        return [a, accessToken.access_token]
+    }, [accessToken])
+}
 
 export const axiosBackend = axios.create({
-    // @ts-ignore
     baseURL: import.meta.env.VITE_API_LINK,
     headers: {
         'Content-Type': 'application/json'
@@ -33,3 +84,18 @@ export const queryClient = new QueryClient({
         },
     }
 })
+
+export const triggerAuthorization = (): AccessToken => {
+    let url = new URL("https://accounts.spotify.com/authorize");
+    let params = new URLSearchParams();
+    params.append("client_id", import.meta.env.VITE_SPOTIFY_CLIENT_ID);
+    params.append("redirect_uri", `${window.location.protocol}//${window.location.host}/spotify-auth-callback`);
+    params.append("scope", scopes.join(','))
+    params.append("state", "4247")
+    params.append("response_type", "code")
+
+    url.search = params.toString();
+    window.location.href = url.toString();
+
+    return {access_token: "", refresh_token: "", expires: 0, token_type: "", expires_in: 0}
+}
---
+++ server/index.ts
@@ -0,0 +1,141 @@
+import expressWsImport from "express-ws";
+import express from "express";
+// @ts-ignore
+import MusicTempo from "music-tempo";
+import _ from "lodash";
+import Bottleneck from "bottleneck";
+import {PrismaClient} from "@prisma/client"
+// @ts-ignore
+import {AudioContext} from "web-audio-api"
+import cors from "cors";
+
+const expressWs = expressWsImport(express());
+const app = expressWs.app;
+const port = 4000
+const context = new AudioContext({sampleRate: 44100});
+const prisma = new PrismaClient()
+
+const limiter = new Bottleneck({
+    minTime: 333,
+    maxConcurrent: 1
+});
+
+app.use(cors())
+
+app.ws('/ws', function (ws, req) {
+    ws.on('message', async (msg) => {
+        const messageJson = JSON.parse(msg.toString())
+        if (messageJson.event === 'extractTempo') {
+            const existing = await prisma.spotifySongTempo.findMany({where: {id: {in: Object.keys(messageJson.data)}}})
+            if (existing.length > 0) {
+                ws.send(JSON.stringify({
+                    event: "tempoExtracted",
+                    data: Object.fromEntries(existing.map(instance => [instance.id, instance.tempo.toFixed(0)]))
+                }))
+            }
+
+            const existingIds = existing.map((instance) => instance.id)
+            const [, toProcess] = _.partition(Object.entries(messageJson.data), ([id, url]: [string, string]) => {
+                return existingIds.includes(id)
+            })
+
+            // @ts-ignore
+            for (const [id, previewUrl] of toProcess) {
+                if (!previewUrl) continue
+                await limiter.schedule(async () => {
+                    await extractTempo(previewUrl, async (tempo: number) => {
+                        try {
+                            await prisma.spotifySongTempo.create({
+                                data: {id: id, tempo: tempo}
+                            })
+                        } catch (e) {
+
+                        }
+                        ws.send(JSON.stringify({
+                            event: "tempoExtracted",
+                            data: {[id]: tempo.toFixed(0)}
+                        }))
+                    })
+                });
+            }
+        }
+    });
+});
+
+app.get('/callback', async (req, res) => {
+    const code = req.query.code;
+
+    if (code === undefined) {
+        res.sendStatus(400)
+    } else {
+        const resp = await fetch(
+            'https://accounts.spotify.com/api/token',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': `Basic ${process.env.SPOTIFY_BASIC_AUTH}`
+                },
+                body: new URLSearchParams([
+                    ['grant_type', 'authorization_code'],
+                    ['code', code.toString()],
+                    ['redirect_uri', `${process.env.FRONTEND_URL}/spotify-auth-callback`]
+                ])
+            }
+        )
+        res.send(
+            await resp.json()
+        )
+    }
+})
+
+app.get('/refresh-token', async (req, res) => {
+    const refresh_token = req.query.refresh_token;
+    if (refresh_token === undefined) {
+        res.sendStatus(400)
+    } else {
+        const resp = await fetch(
+            'https://accounts.spotify.com/api/token',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Authorization': `Basic ${process.env.SPOTIFY_BASIC_AUTH}`
+                },
+                body: new URLSearchParams([
+                    ['grant_type', 'refresh_token'],
+                    ['refresh_token', refresh_token.toString()]
+                ])
+            }
+        )
+        res.send(
+            await resp.json()
+        )
+    }
+})
+
+
+
+const extractTempo = async (previewUrl: string, callback: any) => {
+    console.log(`Extracting tempo for ${previewUrl}`)
+    const audioResponse = await fetch(previewUrl)
+    await context.decodeAudioData(await audioResponse.arrayBuffer(), (buffer: AudioBuffer) => {
+        let audioData = [];
+        if (buffer.numberOfChannels === 2) {
+            const channel1Data = buffer.getChannelData(0);
+            const channel2Data = buffer.getChannelData(1);
+            const length = channel1Data.length;
+            for (let i = 0; i < length; i++) {
+                audioData[i] = (channel1Data[i] + channel2Data[i]) / 2;
+            }
+        } else {
+            audioData = Array.from(buffer.getChannelData(0))
+        }
+        const mt = new MusicTempo(audioData);
+        callback(parseFloat(mt.tempo))
+    })
+}
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
